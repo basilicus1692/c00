b243e6e29718b5da173699ca3b739a36cfc13870
William William <wwilliam@z2t5c4.42bangkok.com>
2025-05-20 18:42:52 +0700

    before home

--- ex01/ft_print_alphabet.c
+++ ex01/ft_print_alphabet.c
@@ -1,20 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wwilliam <wwilliam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/20 17:01:30 by wwilliam          #+#    #+#             */
-/*   Updated: 2025/05/20 17:01:51 by wwilliam         ###   ########.fr       */
+/*   Created: 2025/05/20 17:01:59 by wwilliam          #+#    #+#             */
+/*   Updated: 2025/05/20 17:02:03 by wwilliam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include <unistd.h>
 
-void    ft_putchar(char c)
+void ft_print_alphabet (void)
+
 {
-    write (1, &c, 1);
-    
+    char letter;
+
+    letter = 'a';
+    while (letter <= 'z') {
+        putchar (letter);
+        letter++;
+    }
 }
--- ex02/ft_print_reverse_alphabet.c
+++ ex02/ft_print_reverse_alphabet.c
@@ -1,20 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wwilliam <wwilliam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/20 17:01:30 by wwilliam          #+#    #+#             */
-/*   Updated: 2025/05/20 17:01:51 by wwilliam         ###   ########.fr       */
+/*   Created: 2025/05/20 17:02:07 by wwilliam          #+#    #+#             */
+/*   Updated: 2025/05/20 17:02:11 by wwilliam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include <unistd.h>
 
-void    ft_putchar(char c)
+void	ft_print_reverse_alphabet(void)
 {
-    write (1, &c, 1);
-    
+	char counter;
+
+	counter = 'z';
+	while (counter >= 'a')
+	{
+		write(1, &counter, 1);
+		counter--;
+	}
 }
